23c666efa8621d46a692355dbfd89c84c0b36956
Dylan Clayton <dylanclayton3@gmail.com>
2024-07-08 08:56:25 -0400

    Update l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs

--- framerusercontent.com/sites/3IMKteN1GQ0roUwlfGKmcj/l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs
+++ framerusercontent.com/sites/3IMKteN1GQ0roUwlfGKmcj/l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs
@@ -11507,7 +11507,7 @@ var Xc = {
                                                     style: {
                                                         backgroundImage: "linear-gradient(278deg, var(--token-ae08cd7b-e514-494f-b381-7169722e1c76, rgb(181, 217, 130)) 0%, var(--token-9f3826c5-17f9-4323-8d5c-b246baa048b6, rgb(239, 130, 239)) 100%)"
                                                     },
-                                                    children: "sharp focus "
+                                                    children: "better bets "
                                                 })
                                             })
                                         }),
