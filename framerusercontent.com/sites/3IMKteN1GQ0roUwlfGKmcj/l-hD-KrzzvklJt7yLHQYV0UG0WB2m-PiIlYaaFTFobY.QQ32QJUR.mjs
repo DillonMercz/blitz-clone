c8f5a70ef04474cce193a8a8eedcf2311dfbdd94
Dylan Clayton <dylanclayton3@gmail.com>
2024-07-08 13:42:40 -0400

    Update l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs

--- framerusercontent.com/sites/3IMKteN1GQ0roUwlfGKmcj/l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs
+++ framerusercontent.com/sites/3IMKteN1GQ0roUwlfGKmcj/l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs
@@ -11403,7 +11403,7 @@ var Xc = {
                                     style: {
                                         "--framer-text-alignment": "center"
                                     },
-                                    children: "Backed by years of data, and the best Sports Predicting AI models in the world, custom built by our exceptional team of AI engineers and data scientists."
+                                    children: "Backed by years of data, and the best Sports Predicting AI models in the world, custom built by our exceptional team of AI engineers and data scientists, we achieve above 75% accuracy consistently."
                                 })
                             }),
                             className: "framer-1d0ix5z",
