4a84cc112e280dbb53b9edd58afd3a0311292761
Dylan Clayton <dylanclayton3@gmail.com>
2024-07-14 18:38:57 -0400

    Revert "Update l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs"
    This reverts commit 096f54b5febfa645b3e710632ee51c564ecf98ed.

--- framerusercontent.com/sites/3IMKteN1GQ0roUwlfGKmcj/l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs
+++ framerusercontent.com/sites/3IMKteN1GQ0roUwlfGKmcj/l-hD-KrzzvklJt7yLHQYV0UG0WB2m-PiIlYaaFTFobY.QQ32QJUR.mjs
@@ -11061,7 +11061,7 @@ var Xc = {
                                                         style: {
                                                             "--framer-text-alignment": "left"
                                                         },
-                                                        children: ["We're Not Just Ahead of the Game, We're Rewriting the Rules"]
+                                                        children: ["Our AI Knows the Game Better Than Your Ex!"]
                                                     })
                                                 }),
                                                 initial: le("initial", "1pr5q1g", Pt, "ba39o")
@@ -11082,15 +11082,15 @@ var Xc = {
                                                     style: {
                                                         "--framer-text-alignment": "left"
                                                     },
-                                                    children: "We're Not Just Ahead of the Game, We're Rewriting the Rules"
+                                                    children: "Our AI Knows the Game Better Than Your Ex!"
                                                 })
                                             }),
                                             className: "framer-o7xogh",
                                             "data-framer-appear-id": "o7xogh",
-                                            "data-framer-name": "We're Not Just Ahead of the Game, We're Rewriting the Rules",
+                                            "data-framer-name": "Our AI Knows the Game Better Than Your Ex!",
                                             fonts: ["Inter"],
                                             initial: le("initial", "o7xogh", Pt, "1urlk82"),
-                                            name: "We're Not Just Ahead of the Game, We're Rewriting the Rules!",
+                                            name: "Our AI Knows the Game Better Than Your Ex!",
                                             style: {
                                                 transformPerspective: 1200
                                             },
@@ -11101,17 +11101,17 @@ var Xc = {
                                         children: [e(c, {
                                             __fromCanvasComponent: !0,
                                             className: "framer-159bxrj",
-                                            "data-framer-name": "We're Not Just Ahead of the Game, We're Rewriting the Rules",
+                                            "data-framer-name": "Our AI Knows the Game Better Than Your Ex!",
                                             fonts: ["Inter"],
-                                            name: "We're Not Just Ahead of the Game, We're Rewriting the Rules",
+                                            name: "Our AI Knows the Game Better Than Your Ex!",
                                             verticalAlignment: "top",
                                             withExternalLayout: !0
                                         }), e(c, {
                                             __fromCanvasComponent: !0,
                                             className: "framer-kn664x",
-                                            "data-framer-name": "We're Not Just Ahead of the Game, We're Rewriting the Rules",
+                                            "data-framer-name": "Our AI Knows the Game Better Than Your Ex!",
                                             fonts: ["Inter"],
-                                            name: "We're Not Just Ahead of the Game, We're Rewriting the Rules",
+                                            name: "Our AI Knows the Game Better Than Your Ex!",
                                             verticalAlignment: "top",
                                             withExternalLayout: !0
                                         })]
